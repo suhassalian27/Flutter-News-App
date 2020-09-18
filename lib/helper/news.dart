d482ef91fded2135b3f8f33aebfe36a3ed8a35f4
Suhas Salian <suhas.salian27@gmail.com>
2020-09-18 23:07:07 +0530

    helper files

---
+++ lib/helper/news.dart
@@ -0,0 +1,33 @@
+import 'dart:convert';
+
+import 'package:flutternewsapp/model/article_model.dart';
+import 'package:http/http.dart' as http;
+
+class News {
+  List<ArticleModel> news = [];
+
+  Future<void> getNews() async {
+    String url =
+        "http://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=f67fad99b8e64920a13a5fc0deecb1d0";
+
+    var response = await http.get(url);
+
+    var jsonData = jsonDecode(response.body);
+
+    if (jsonData["status"] == "ok") {
+      jsonData["articles"].forEach((element) {
+        if (element["urlToImage"] != null && element["description"] != null) {
+          ArticleModel articleModel = ArticleModel(
+              title: element['title'],
+              author: element['author'],
+              description: element['description'],
+              url: element['url'],
+              urlToImage: element['urlToImage'],
+              publishedAt: element['publishedAt'],
+              content: element['content']);
+          news.add(articleModel);
+        }
+      });
+    }
+  }
+}
